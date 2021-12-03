d44b2a1ae28c3f74490a26bc00491902303531de
jorgeAP <jorgeagoiz@gmail.com>
2021-12-03 16:47:12 +0100

    Learning Python

---
+++ errorHandling.py
@@ -0,0 +1,17 @@
+# Error Handling
+adult = False
+
+while adult == False:
+    try:
+        age = int(input("What is your age?: "))
+        if age > 17:
+            adult = True
+            print("Thank You!!")
+            break
+        else:
+            print("You must have 18 or more!!")
+            continue
+    except ValueError:
+        print("Please enter a number")
+    except ZeroDivisionError:
+        print("please enter age higher than 0")
